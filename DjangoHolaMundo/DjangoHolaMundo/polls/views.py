e63820037e87620a5789f651bb5619150f0c0f6f
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2025-09-09 12:00:16 -0600

    Se genero la pagina result. Se implento clases de Vista

--- DjangoHolaMundo/DjangoHolaMundo/polls/views.py
+++ DjangoHolaMundo/DjangoHolaMundo/polls/views.py
@@ -4,6 +4,14 @@ from django.urls import reverse
 from .models import Question, Choice
 from django.template import loader
 from django.db.models import F
+from django.views import generic
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
 
 # Create your views here.
 # def index(request):
@@ -36,9 +44,9 @@ def detail(request, question_id):
     
         
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+def results(request, question_id): 
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
