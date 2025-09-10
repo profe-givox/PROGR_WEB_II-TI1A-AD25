ac28ee4ee7ef3ba07c9613604ed13acf6661d8f5
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2025-09-10 11:59:03 -0600

    Usamos Vistas de clase genericas y agregamos estaticos a la app de un proyecto.

--- DjangoHolaMundo/DjangoHolaMundo/polls/urls.py
+++ DjangoHolaMundo/DjangoHolaMundo/polls/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     #path('', views.index , name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
     # path('<int:question_id>/results/', views.results, name='results'),
-    # path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('<int:pk>/results/', views.ResultView.as_view(), name='results'),
+    # path('<int:pk>/', views.GatoViewDetail.as_view(), name='detail'),
 ]
--- DjangoHolaMundo/DjangoHolaMundo/polls/views.py
+++ DjangoHolaMundo/DjangoHolaMundo/polls/views.py
@@ -6,13 +6,20 @@ from django.template import loader
 from django.db.models import F
 from django.views import generic
 
+class GatoViewDetail (generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultView (generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
+    queryset = Question.objects.order_by('-pub_date')[:5]
+    
 
-    def get_queryset(self):
-        return Question.objects.order_by("-pub_date")[:5]
-
+    
 # Create your views here.
 # def index(request):
 #     lastest_question_list =  Question.objects.order_by('-pub_date')[:5]
