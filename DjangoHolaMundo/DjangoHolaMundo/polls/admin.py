10553ecf1ebe3bf7c05f00fe1b94e0976908c77e
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2025-09-12 12:00:42 -0600

    agregamos fondo y agregamos question admin, choise (se personalizo el admin site utilizando stack in line)

--- DjangoHolaMundo/DjangoHolaMundo/polls/admin.py
+++ DjangoHolaMundo/DjangoHolaMundo/polls/admin.py
@@ -3,4 +3,22 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question, Choice
 
-admin.site.register(Question)
+##class ChoiceInline(admin.TabularInline): ...
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+class QuestionModelAdmin(admin.ModelAdmin):
+    ##fields = ['pub_date', 'question_text']
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+    inlines = [ChoiceInline]
+
+    
+
+##admin.site.register(Question)
+admin.site.register(Question, QuestionModelAdmin)
+admin.site.register(Choice)
