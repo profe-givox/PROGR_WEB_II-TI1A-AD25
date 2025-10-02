e8e414cc9ab8f4b09ed3e46594b1b2926de5c7c4
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2025-10-02 12:01:46 -0600

    Se creo la app snippet y archivo requerients para esta app

--- tutorial/tutorial/tutorial/tutorial/settings.py
+++ tutorial/tutorial/tutorial/tutorial/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-
+    'snippets',
     'rest_framework',
     'quickstart',
     'django.contrib.admin',
@@ -123,3 +123,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 1
+}
