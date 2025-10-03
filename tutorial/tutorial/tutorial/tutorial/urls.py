12e0f315abde85a281aaa26d49df0b09bef15693
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2025-10-03 12:05:01 -0600

    Se agrego el modelo, serializador y vista de funcion para generar el endpoint del recurso snippet de la app Sippets

--- tutorial/tutorial/tutorial/tutorial/urls.py
+++ tutorial/tutorial/tutorial/tutorial/urls.py
@@ -30,6 +30,7 @@ urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
+    path('', include('snippets.urls')),
 
 
 ]
