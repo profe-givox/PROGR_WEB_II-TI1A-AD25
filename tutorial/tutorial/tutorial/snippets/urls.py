340aede08ddb4f16a1494ec34a010d1b2ee89c08
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2025-10-16 11:12:55 -0600

    Se versiono el código por primera vez Tema:Ajax Proyecto: Peticiones Javascript AJAX (asyncronas) a una app django.

--- tutorial/tutorial/tutorial/snippets/urls.py
+++ tutorial/tutorial/tutorial/snippets/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 from snippets import views
 
 urlpatterns = [
-    path('snippets/', views.snippet_list),
-    path('snippets/<int:pk>/', views.snippet_detail),
+    # path('snippets/', views.snippet_list),
+    # path('snippets/<int:pk>/', views.snippet_detail),
+
+    # path('snippets/', views.snippet_list_apiview),
+    # path('snippets/<int:pk>/', views.snippet_detail_apiview),
+
+    path('snippets/', views.SnippetList_apiviewclass.as_view()),
+    path('snippets/<int:pk>/', views.SnippetDetail_apiviewclass.as_view()),
+
 ]
